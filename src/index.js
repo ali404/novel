a8646a2ea15b58a8080c0d0537ea2b2050f56ff7
ali404 <acerrahoglu.ali@gmail.com>
2018-01-03 17:46:47 +0200

    preparing to change from create-react-app to electron-react-redux-boilerplate

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import {render} from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-
-import routes from './routes'
-
-render(routes, document.getElementById('novel-app'))
-registerServiceWorker()
